8f77c94d4c563e13897f9e203d0effa989911f09
Lauren Dorman <hello@laurendorman.io>
2019-09-26 23:34:26 +0200

    feat: add past event data

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Layout from "../components/layout";
 import SEO from "../components/seo"
-import { injectIntl } from "gatsby-plugin-intl";
+import { injectIntl, defineMessages } from "gatsby-plugin-intl";
 
 const Events = ({ intl }) => {
   return (
     <Layout>
       <SEO title={intl.formatMessage({ id: 'events_title' })} />
+
       <h1>{intl.formatMessage({ id: 'past_events_label' })}</h1>
+
+      <h2>{intl.formatMessage({ id: 'past_events.0.title' })}</h2>
+      <time>{intl.formatMessage({ id: 'past_events.0.date' })}</time>
+      <h2>{intl.formatMessage({ id: 'past_events.1.title' })}</h2>
+      <time>{intl.formatMessage({ id: 'past_events.1.date' })}</time>
     </Layout>
   );
 };
